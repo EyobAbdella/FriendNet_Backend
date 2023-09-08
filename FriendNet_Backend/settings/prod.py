3dbf91605b14d13a43c7ed02c60c470b5114fec3
Eyob Abdella <eyobabdellasharo@gmail.com>
2023-09-08 16:40:16 -0500

    Make correct env.

--- FriendNet_Backend/settings/prod.py
+++ FriendNet_Backend/settings/prod.py
@@ -2,11 +2,16 @@ import os
 import dj_database_url
 from .common import *
 
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
 DEBUG = False
 
-SECRET_KEY = os.environ['SECRET_KEY']
+SECRET_KEY = os.environ["SECRET_KEY"]
 
-ALLOWED_HOSTS = ['.vercel.app']
+ALLOWED_HOSTS = ["*"]
 
 # HTTPS Settings
 SESSION_COOKIE_SECURE = True
@@ -14,25 +19,31 @@ CSRF_COOKIE_SECURE = True
 SECURE_SSL_REDIRECT = True
 
 # HSTS Settings
-SECURE_HSTS_SECONDS = 31536000 # 1 year
+SECURE_HSTS_SECONDS = 31536000  # 1 year
 SECURE_HSTS_PRELOAD = True
 SECURE_HSTS_INCLUDE_SUBDOMAINS = True
 
 
-DATABASES = {
-    'default': dj_database_url.config()
-}
+DATABASES = {"default": dj_database_url.config()}
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.mysql",
+#         "NAME": os.environ.get("DB_NAME"),
+#         "USER": os.environ.get("DB_USER"),
+#         "PASSWORD": os.environ.get("DB_PASSWORD"),
+#         "HOST": os.environ.get("DB_HOST"),
+#         "PORT": os.environ.get("DB_PORT"),
+#     }
+# }
 
-REDIS_URL = os.environ['REDIS_URL']
+REDIS_URL = os.environ["REDIS_URL"]
 
 CHANNEL_LAYERS = {
     "default": {
         "BACKEND": "channels_redis.core.RedisChannelLayer",
         "CONFIG": {
-            "hosts": REDIS_URL,
+            "hosts": [REDIS_URL],
         },
     },
 }
-
-
